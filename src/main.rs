c03b58edc7df2c8d767939e6e3d3d81b00c3666c
Brandon White <bwhite@mdlogic.com>
2024-06-13 21:55:56 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod textures;
+
+use common::dbl_buffer::DoubleBuffer;
+
+fn main() {
+    let mut double_buffer = DoubleBuffer::new(1024);
+
+    // Simulate writing to the inactive buffer
+    double_buffer.write_to_inactive_buffer(vec![1, 2, 3, 4]);
+
+    // Swap the buffers
+    double_buffer.swap_buffers();
+
+    // Now the previously inactive buffer is active
+    let active_buffer = double_buffer.get_active_buffer();
+    let buffer_content = active_buffer.lock().unwrap();
+    println!("{:?}", *buffer_content);
+}
